b49fbd6900f62674985b83a780b3cafd19983c8c
daks-dev <mail@daks.dev>
2023-03-25 22:48:08 +0300

    +

--- src/routes/portfolio/+page.server.ts
+++ src/routes/portfolio/+page.server.ts
@@ -1,4 +1,4 @@
-import getter from '$lib/assets/images/content/portfolio';
+import getter from '$lib/assets/images/content/portfolio/index.async';
 
 import type { PageServerLoad } from './$types'
 export const load: PageServerLoad = async () => ({
